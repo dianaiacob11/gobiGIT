7c8b5ecd72a8d86c4ddb9b095901ec3ed3042c78
Diana Iacob <diana_iacob11@yahoo.com>
2015-03-02 14:25:28 +0100

    1 day - parse MGI

--- parseMGI.pl
+++ parseMGI.pl
@@ -5,6 +5,8 @@ use diagnostics;
 
 use Getopt::Long;
 use LWP::Simple;
+use Digest::MD5;
+my $ctx = Digest::MD5->new;
 
 my ($dir, $debug);
 if (@ARGV < 1){ die "Usage: $0 --dir <path_to_MGI_file_directory> [--d|debug]\n";}
@@ -32,8 +34,13 @@ foreach my $file (@dir) {
         my $chr               = defined($line_array[3]) ? $line_array[3] : "" ;
         my $allele_type       = defined($line_array[5]) ? $line_array[5] : "" ;
         my $allele_attributes = defined($line_array[6]) ? $line_array[6] : "" ;
+        my $transmission      = defined($line_array[7]) ? $line_array[7] : "" ;
+        my $phenotype         = defined($line_array[8]) ? $line_array[8] : "" ;
         
-        print $allele_id."\n";
+        #print $allele_id."\n".$allele_name."\n".$chr."\n".$allele_type."\n".$allele_attributes."\n".$transmission."\n".$phenotype."\n";
+        #insert in mgi db
+        
+        &parsePhenotype($phenotype, $allele_id);
     }
 
 }
@@ -43,6 +50,30 @@ sub usage{
     print "Usage: ./parseMGI --dir <path_to_MGI_file_directory> [--d|debug]\n";
 }
 
+sub parsePhenotype{
+    my $pheno_string = $_[0]; # e.g. growth/size/body | homeostasis | limbs/digits/tail | skeleton
+    my $allele_id    = $_[1]; # e.g. MGI:3604810
+    my @pheno_array  = split(/\s+|\s+/, $pheno_string);
+    foreach my $phenotype (@pheno_array)
+    {
+        if($phenotype ne "|")
+        {
+            my $phenotype_id = &getMD5ForPhenotype($phenotype);
+            print $phenotype."\n".$phenotype_id."\n".$allele_id."\n";
+            # insert in mgi_phenotype db
+        }
+    }
+
+}
+
+sub getMD5ForPhenotype{
+    my $phenotype = $_[0];
+    
+    $ctx->add($phenotype);
+    my $digest = $ctx->hexdigest;
+    
+    return $digest;
+}
 
 __END__
 #TODO: write pom
