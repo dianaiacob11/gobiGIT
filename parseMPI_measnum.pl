93d3f1fd2fdd6978e43cd657ff9304fe4945cfd2
weletonne <aberatis@t-online.de>
2015-03-02 16:34:04 +0100

    INSERT FUNCTION ADDED

--- parseMPI_measnum.pl
+++ parseMPI_measnum.pl
@@ -6,6 +6,9 @@ use diagnostics;
 use Getopt::Long;
 use LWP::Simple;
 
+my $database = "gobi";
+my $db = &connectToDB($database);
+
 my ($file, $debug);
 if (@ARGV < 1){ die "Usage: $0 --file <path_to_measnum_file> [--d|debug]\n";}
 
@@ -30,12 +33,35 @@ foreach my $line (@fileContent){
     my $measnum    = defined($line_array[0]) ? $line_array[0] : '' ;
 
     
-    print $phenotype.",  ".$measnum."\n";
+    #print $phenotype.",  ".$measnum."\n";
+    insertToDB($measnum,$phenotype);
 }
 
 sub usage{
     print "Incorrect parameters \n";
     print "Usage: ./parseMPI_meansnum.pl --file <path_to_measnum_file> [--d|debug]\n";
+}
+
+sub connectToDB{
+    
+    my $database    = $_[0];
+    my $host        = "164.177.170.83";
+    my $user        = "root";
+    my $pw          = "";
+    
+    my $dsn         = "dbi:mysql:$database:$host";
+    my $dbh = DBI->connect($dsn, $user, $pw) or die "Error connecting to database.";
+    
+    return $dbh;
+}
+
+sub insertToDB{
+    
+    my $dbTable = "mpi_measnum";
+    my $query = "INSERT IGNORE INTO ".$dbTable."(measnum, phenotype) VALUES (?,?)";
+    print $query."\n";
+    my $sth = $db->prepare($query);
+    $sth->execute($_[0], $_[1]);    
 }
 
 
