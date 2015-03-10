471e9f4cfda53641f720b23e5f86381b3fd82f0d
Diana Iacob <diana_iacob11@yahoo.com>
2015-03-10 10:09:09 +0100

    changes

--- MODULES/PlotsExtract.pm
+++ MODULES/PlotsExtract.pm
@@ -164,7 +164,7 @@ sub                 get_analysisMPI_phenotypes_nr_zscore2{
     my $count      = '';
 
     while(@row = $sth->fetchrow_array){
-        if($row[2] > 2){
+        if($row[2] > 25000){
             $phenotypes  = join(',', $phenotypes, $row[0]);
             chomp($phenotypes);
             
